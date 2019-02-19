18d5a1223b9b6692fcc1c57158301fb82e39dc53
cnguyen94 <44678946+cnguyen94@users.noreply.github.com>
2019-02-18 18:14:48 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/* 
+ * File:   main.cpp
+ * Author: Cuong Nguyen
+ * Created on February 17, 2019, 10:00 AM
+ * Purpose: Hello World
+ */
+
+//System Libraries
+
+#include <iostream>
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+
+int main(int argc, char** argv) {  
+    
+//Output
+    cout<<"Hello, World!!!"<<endl;
+
+//Exit program
+    return 0;
+}
+
+   
